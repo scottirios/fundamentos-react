f13aada9afa5dbd248cbecc981da97e3a5641aec
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-07-29 22:40:30 -0300

    feat: create page login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,20 @@
+import { Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
+
+import loginImage from "../../public/assets/login-image.gif";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Flex w="100vw" h="100vh">
+      <Flex w="50%" bg="#2C73EB" align={"center"} justify="center">
+        <Image w={500} h={500} src={loginImage.src} />
+      </Flex>
+      <Stack w="50%" bg="white">
+        <Heading>Account Login</Heading>
+        <Text>
+          If you are already a member you can login with your email address and
+          password.
+        </Text>
+      </Stack>
+    </Flex>
+  );
 }
