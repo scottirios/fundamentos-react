2927cb5ee83240dfbbd0af20a177b72149429be9
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-08-19 21:54:36 -0300

    feat: importando icones na pagina

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,52 @@
+import { Flex, Group, IconButton, Image, Separator } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import {
+  IoMdHelpCircle,
+  IoMdMenu,
+  IoMdNotifications,
+  IoMdSettings,
+} from "react-icons/io";
+import { Avatar } from "@/components/ui/avatar";
+import logo from "../../public/assets/logo-preta.png";
 
 type DefaultLayoutProps = {
   children: ReactNode;
 };
 
 export function DefaultLayout({ children }: DefaultLayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <Flex flexDir="column" bg="gray.50">
+      <Flex justify="space-between" w="full" py={2} px={4}>
+        <Flex align="center" gap={4}>
+          <IconButton variant="outline">
+            <IoMdMenu />
+          </IconButton>
+          <Image w="130px" height="60px" src={logo.src} />
+        </Flex>
+
+        <Group>
+          <IconButton variant="outline">
+            <IoMdNotifications />
+          </IconButton>
+
+          <IconButton variant="outline">
+            <IoMdHelpCircle />
+          </IconButton>
+
+          <IconButton variant="outline">
+            <IoMdSettings />
+          </IconButton>
+
+          <Separator orientation="vertical" h="full" />
+
+          <Avatar
+            name="Lucas Scotti"
+            src="https://avatars.githubusercontent.com/u/128654904?v=4"
+            colorPalette="purple"
+          />
+        </Group>
+      </Flex>
+      {children}
+    </Flex>
+  );
 }
