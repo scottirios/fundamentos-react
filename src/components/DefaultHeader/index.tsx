99c03febd9e90de7e2daca937ed4fa5840a40ccd
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-08-21 19:16:22 -0300

    feat: create component DefaultHeader

---
+++ src/components/DefaultHeader/index.tsx
@@ -0,0 +1,67 @@
+import { Flex, Group, IconButton, Image, Separator } from "@chakra-ui/react";
+import {
+  IoIosMenu,
+  IoMdHelpCircle,
+  IoMdMoon,
+  IoMdNotifications,
+  IoMdSettings,
+  IoMdSunny,
+} from "react-icons/io";
+import { Avatar } from "@/components/ui/avatar";
+import { useColorMode } from "@/components/ui/color-mode";
+import logoBranco from "../../../public/assets/logo-branca.png";
+import logoPreto from "../../../public/assets/logo-preta.png";
+
+export function DefaultHeader() {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <Flex flexDir="column">
+      <Flex
+        w="full"
+        justify="space-between"
+        borderBottomWidth={1}
+        py={2}
+        px={4}
+      >
+        <Flex align="center" gap={4}>
+          <IconButton variant="outline">
+            <IoIosMenu />
+          </IconButton>
+
+          {colorMode === "dark" ? (
+            <Image w="130px" h="60px" src={logoBranco.src} />
+          ) : (
+            <Image w="115px" h="60px" src={logoPreto.src} />
+          )}
+        </Flex>
+
+        <Group>
+          <IconButton variant="ghost" rounded="full">
+            <IoMdNotifications />
+          </IconButton>
+
+          <IconButton variant="ghost" rounded="full">
+            <IoMdHelpCircle />
+          </IconButton>
+
+          <IconButton variant="ghost" rounded="full">
+            <IoMdSettings />
+          </IconButton>
+
+          <IconButton variant="ghost" rounded="full" onClick={toggleColorMode}>
+            {colorMode === "dark" ? <IoMdMoon /> : <IoMdSunny />}
+          </IconButton>
+
+          <Separator h={6} colorPalette="gray" orientation="vertical" mx={2} />
+
+          <Avatar
+            name="Gabriel Biankati"
+            colorPalette="purple"
+            src="https://avatars.githubusercontent.com/u/128652887?v=4"
+          />
+        </Group>
+      </Flex>
+    </Flex>
+  );
+}
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,66 +1,16 @@
-import { Flex, Group, IconButton, Image, Separator } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { ReactNode } from "react";
-import {
-  IoMdHelpCircle,
-  IoMdMenu,
-  IoMdMoon,
-  IoMdNotifications,
-  IoMdSettings,
-  IoMdSunny,
-} from "react-icons/io";
-import { Avatar } from "@/components/ui/avatar";
-import { useColorMode } from "@/components/ui/color-mode";
-import logoBranca from "../../public/assets/logo-branca.png";
-import logoPreto from "../../public/assets/logo-preta.png";
+import { DefaultHeader } from "@/components/DefaultHeader";
 
 type DefaultLayoutProps = {
   children: ReactNode;
 };
 
 export function DefaultLayout({ children }: DefaultLayoutProps) {
-  const { toggleColorMode, colorMode } = useColorMode();
-
   return (
     <Flex flexDir="column">
-      <Flex justify="space-between" w="full" borderWidth={1} py={2} px={4}>
-        <Flex align="center" gap={4}>
-          <IconButton variant="outline">
-            <IoMdMenu />
-          </IconButton>
+      <DefaultHeader />
 
-          {colorMode === "dark" ? (
-            <Image w="130px" height="60px" src={logoBranca.src} />
-          ) : (
-            <Image w="115px" height="60px" src={logoPreto.src} />
-          )}
-        </Flex>
-
-        <Group>
-          <IconButton variant="ghost" rounded="full">
-            <IoMdNotifications />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full">
-            <IoMdHelpCircle />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full">
-            <IoMdSettings />
-          </IconButton>
-
-          <IconButton variant="ghost" rounded="full" onClick={toggleColorMode}>
-            {colorMode === "dark" ? <IoMdMoon /> : <IoMdSunny />}
-          </IconButton>
-
-          <Separator orientation="vertical" h={6} mx={2} />
-
-          <Avatar
-            name="Lucas Scotti"
-            src="https://avatars.githubusercontent.com/u/128654904?v=4"
-            colorPalette="purple"
-          />
-        </Group>
-      </Flex>
       {children}
     </Flex>
   );
