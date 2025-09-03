9775d64b14202bf8e303e6209b71a8c6f9f56b41
Lucas Scotti <lucas_scotti.13@hotmail.com>
2025-09-02 21:25:50 -0300

    feat: Adicionando defaultChecked na navegacao do Menu

--- src/components/DefaultHeader/Menu.tsx
+++ src/components/DefaultHeader/Menu.tsx
@@ -5,12 +5,13 @@ import {
   Portal,
   VStack,
 } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { FaGraduationCap, FaUserPlus } from "react-icons/fa6";
 import { IoIosBook, IoMdMenu, IoMdPeople } from "react-icons/io";
 import { IoCalendarClear, IoLogOut } from "react-icons/io5";
 import { PiFileTextFill } from "react-icons/pi";
 import { RiDashboardFill } from "react-icons/ri";
+import { signOut } from "@/contexts/SessionContext";
 import { NavigationItem } from "./NavigationItem";
 
 export function Menu() {
@@ -37,48 +38,59 @@ export function Menu() {
                   icon={<RiDashboardFill />}
                   label="Dashboard"
                   onClick={() => router.push("/")}
+                  defaultChecked={router.asPath === "/"}
                 />
 
                 <NavigationItem
                   icon={<IoMdPeople />}
                   label="Estudantes"
                   onClick={() => router.push("/students")}
+                  defaultChecked={router.asPath === "/students"}
                 />
 
                 <NavigationItem
                   icon={<FaGraduationCap />}
                   label="Professores"
                   onClick={() => router.push("/teachers")}
+                  defaultChecked={router.asPath === "/teachers"}
                 />
 
                 <NavigationItem
                   icon={<IoIosBook />}
                   label="Cursos"
                   onClick={() => router.push("/courses")}
+                  defaultChecked={router.asPath === "/courses"}
                 />
 
                 <NavigationItem
                   icon={<PiFileTextFill />}
                   label="Notas"
                   onClick={() => router.push("/grades")}
+                  defaultChecked={router.asPath === "/grades"}
                 />
 
                 <NavigationItem
                   icon={<FaUserPlus />}
                   label="Matrículas"
                   onClick={() => router.push("/enrollment")}
+                  defaultChecked={router.asPath === "/enrollment"}
                 />
 
                 <NavigationItem
                   icon={<IoCalendarClear />}
                   label="Calendário"
                   onClick={() => router.push("/calendar")}
+                  defaultChecked={router.asPath === "/calendar"}
                 />
               </VStack>
             </Drawer.Body>
 
             <Drawer.Footer>
-              <NavigationItem icon={<IoLogOut />} label="Sair" />
+              <NavigationItem
+                icon={<IoLogOut />}
+                label="Sair"
+                onClick={signOut}
+              />
             </Drawer.Footer>
             <Drawer.CloseTrigger asChild>
               <CloseButton size="sm" />
